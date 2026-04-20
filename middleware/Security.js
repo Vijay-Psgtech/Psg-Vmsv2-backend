31efc6fd4c85cecfec8d39748dfd994b5d09d80b
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-04-20 10:07:14 +0530

    Security middleWare file cors origin url's has been added.

--- middleware/Security.js
+++ middleware/Security.js
@@ -51,13 +51,9 @@ export function corsConfig() {
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:5100",
+        "http://localhost:5000",
         "http://localhost:3000",
         "http://localhost:3001",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-        "http://127.0.0.1:5100",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:3001",
         "https://vmstest.psginstitutions.in",
       ];
 
